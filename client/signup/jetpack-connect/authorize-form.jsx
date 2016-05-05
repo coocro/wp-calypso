8fd7d43de609bffc6d6ac1c7b047436cab444126
johnHackworth <javier.alvarez.lop@gmail.com>
2016-05-05 17:11:30 +0200

    Jetpack Connect: redirect to plans page

--- client/signup/jetpack-connect/authorize-form.jsx
+++ client/signup/jetpack-connect/authorize-form.jsx
@@ -30,9 +30,10 @@ import LocaleSuggestions from 'signup/locale-suggestions';
 import { recordTracksEvent } from 'state/analytics/actions';
 
 /**
- * Module variables
+ * Constants
  */
-const STATS_PAGE = '/stats/insights/';
+
+const PLANS_PAGE = '/jetpack/connect/plans/';
 const authUrl = '/wp-admin/admin.php?page=jetpack&connect_url_redirect=true&calypso_env=' + process.env.NODE_ENV;
 const JETPACK_CONNECT_TTL = 60 * 60 * 1000; // 1 Hour
 
@@ -150,6 +151,10 @@ const LoggedInForm = React.createClass( {
 			! props.calypsoStartedConnection &&
 			queryObject.redirect_after_auth ) {
 			this.props.goBackToWpAdmin( queryObject.redirect_after_auth );
+		} else if ( authorizeSuccess &&
+			! isRedirectingToWpAdmin &&
+			props.calypsoStartedConnection ) {
+			page( this.getRedirectionTarget() );
 		}
 	},
 
@@ -253,7 +258,7 @@ const LoggedInForm = React.createClass( {
 		if ( this.props.calypsoStartedConnection ) {
 			const site = this.props.jetpackConnectAuthorize.queryObject.site;
 			const siteSlug = site.replace( /^https?:\/\//, '' ).replace( /\//g, '::' );
-			return STATS_PAGE + siteSlug;
+			return PLANS_PAGE + siteSlug;
 		}
 
 		return queryObject.redirect_after_auth;
